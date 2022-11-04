09d528b521454c3d750d332561853895264af29f
George Abraham <41584300+ig-georgeA@users.noreply.github.com>
2022-11-04 16:41:54 -0400

    tabs-testing updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './error-routing/not-found/not-found.component';
 import { UncaughtErrorComponent } from './error-routing/error/uncaught-error.component';
 import { ErrorRoutingModule } from './error-routing/error-routing.module';
-import { MasterViewComponent } from './master-view/master-view.component';
+import { CustTabsComponent } from './cust-tabs/cust-tabs.component';
+import { MulltiTabsComponent } from './mullti-tabs/mullti-tabs.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'master-view', pathMatch: 'full' },
+  { path: '', redirectTo: 'cust-tabs', pathMatch: 'full' },
   { path: 'error', component: UncaughtErrorComponent },
-  { path: 'master-view', component: MasterViewComponent, data: { text: 'Master View' } },
+  { path: 'cust-tabs', component: CustTabsComponent, data: { text: 'Cust_tabs' } },
+  { path: 'mullti-tabs', component: MulltiTabsComponent, data: { text: 'Mullti tabs' } },
   { path: '**', component: PageNotFoundComponent } // must always be last
 ];
 
